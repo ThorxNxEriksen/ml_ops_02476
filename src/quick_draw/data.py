4c8d0087d268377be5e175b0e15157b8eebfe27c
clarachristiansen <s214659@dtu.dk>
2025-01-14 11:08:57 +0100

    split data

--- src/quick_draw/data.py
+++ src/quick_draw/data.py
@@ -1,29 +1,74 @@
 from pathlib import Path
-
+import torch
+from sklearn.model_selection import train_test_split
 import typer
 from torch.utils.data import Dataset
+from typing import Callable, Optional, Tuple, Union, List 
+from torch.utils.data import TensorDataset
 
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+def split_data(tensor, train_ratio, val_ratio):
+    train_val_data, test_data = train_test_split(tensor, test_size=1 - (train_ratio + val_ratio))
+    train_data, val_data = train_test_split(train_val_data, test_size=val_ratio / (train_ratio + val_ratio))
+    return train_data, val_data, test_data
 
-    def __init__(self, raw_data_path: Path) -> None:
-        self.data_path = raw_data_path
 
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
+def preprocess(categories: List[str], train_ratio: float, val_ratio: float):
+    final_data = {
+            'all_train_images' : [],
+            'all_val_images' : [],
+            'all_test_images' : [],
+            'all_train_targets' : [],
+            'all_val_targets' : [],
+            'all_test_targets' : []}
+    
+    category_to_label = {category: idx for idx, category in enumerate(categories)}
 
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
+    for category in categories:
+        print(category)
+        data = torch.load(f'data/raw/{category}.pt', weights_only=True)
+        
+        train_images, val_images, test_images = split_data(data, train_ratio, val_ratio)
 
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
+        label = category_to_label[category]
+        train_targets = torch.full((len(train_images),), label, dtype=torch.long)
+        val_targets = torch.full((len(val_images),), label, dtype=torch.long)
+        test_targets = torch.full((len(test_images),), label, dtype=torch.long)
 
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    dataset = MyDataset(raw_data_path)
-    dataset.preprocess(output_folder)
+        final_data['all_train_images'].append(train_images)
+        final_data['all_val_images'].append(val_images)
+        final_data['all_test_images'].append(test_images)
 
+        final_data['all_train_targets'].append(train_targets)
+        final_data['all_val_targets'].append(val_targets)
+        final_data['all_test_targets'].append(test_targets)
+
+    # Merge all splits
+    train_images_merged = torch.cat(final_data['all_train_images'])
+    train_targets_merged = torch.cat(final_data['all_train_targets'])
+
+    val_images_merged = torch.cat(final_data['all_val_images'])
+    val_targets_merged = torch.cat(final_data['all_val_targets'])
+
+    test_images_merged = torch.cat(final_data['all_test_images'])
+    test_targets_merged = torch.cat(final_data['all_test_targets'])
+
+    # Create TensorDatasets
+    train_dataset = TensorDataset(train_images_merged, train_targets_merged)
+    val_dataset = TensorDataset(val_images_merged, val_targets_merged)
+    test_dataset = TensorDataset(test_images_merged, test_targets_merged)
+
+    # Save TensorDatasets
+    torch.save(train_dataset, 'data/processed/train_dataset.pt')
+    torch.save(val_dataset, 'data/processed/val_dataset.pt')
+    torch.save(test_dataset, 'data/processed/test_dataset.pt')
+
+def load_dataset(dataset_name: str):
+    dataset = torch.load(f'data/processed/{dataset_name}_dataset.pt', weights_only=False)   
+    return dataset   
 
 if __name__ == "__main__":
-    typer.run(preprocess)
+    categories = ['bear', 'broccoli', 'cake', 'cloud', 'bush', 'The Mona Lisa', 'The Great Wall of China', 'sea turtle', 'moustache', 'mouth']
+    preprocess(categories, 0.7, 0.15)
+    load_dataset('train')
+    #typer.run(preprocess)
