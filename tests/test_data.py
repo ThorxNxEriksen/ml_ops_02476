f439b5ac0d30d21975935f5666b0cd716b465fe9
Søren Mondrup <soerenmondrup@gmail.com>
2025-01-15 11:34:26 +0100

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,7 +7,7 @@ from src.quick_draw.data import load_dataset
 from tests import _PATH_DATA
 
 # Define the path to the processed train dataset
-train_dataset_path = os.path.join(_PATH_DATA, 'processed', 'train_dataset.pt')
+train_dataset_path = os.path.abspath(os.path.join(_PATH_DATA, 'processed', 'train_dataset.pt'))
 
 @pytest.mark.skipif(not os.path.exists(train_dataset_path), reason="Processed datasets not found")
 def test_my_dataset():
