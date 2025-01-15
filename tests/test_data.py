5708c56c9670fb96fa26d8217964b2157c8b936c
Søren Mondrup <soerenmondrup@gmail.com>
2025-01-15 11:20:44 +0100

    Updating tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,18 @@
+import os
+
+import pytest
 from torch.utils.data import Dataset
 
-from quick_draw.data import load_dataset
+from src.quick_draw.data import load_dataset
+from tests import _PATH_DATA
 
+# Define the path to the processed train dataset
+train_dataset_path = os.path.join(_PATH_DATA, 'processed', 'train_dataset.pt')
+
+@pytest.mark.skipif(not os.path.exists(train_dataset_path), reason="Processed datasets not found")
 def test_my_dataset():
     """Test the MyDataset class."""
-    train_set = load_dataset('train')
-    val_set = load_dataset('val')
+    train_set = load_dataset("train")
+    val_set = load_dataset("val")
     assert isinstance(train_set, Dataset)
     assert isinstance(val_set, Dataset)
