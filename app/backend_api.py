13482c59eec7e5a9eb0aa52efb9385e9bfdf1515
ThorxNxEriksen <thor.n.eriksen@gmail.com>
2025-01-21 17:14:51 +0100

    TNE/dockerfile now running the api. Backend moved to app/

--- app/backend_api.py
+++ app/backend_api.py
@@ -5,7 +5,21 @@ import torchvision.transforms as transforms
 from PIL import Image
 import io
 import os
-from model import QuickDrawModel
+import sys
+
+
+#How to run it 
+# curl -X 'POST' \
+#    'http://127.0.0.1:8000/predict' \
+#    -H 'accept: application/json' \
+#    -H 'Content-Type: multipart/form-data' \
+#    -F 'file=@/mnt/c/Users/ThorNørgaardEriksen/OneDrive - Intellishore/3. Privat/2. Studie/2. Kurser/mlOps 02476/wall_test.png;type=image/png'
+
+
+# Add src to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
+from quick_draw.model import QuickDrawModel
+
 
 app = FastAPI()
 
@@ -24,7 +38,7 @@ CATEGORIES = ['bear', 'broccoli', 'cake', 'cloud', 'bush',
 
 # Load model
 model = QuickDrawModel(num_classes=len(CATEGORIES))
-model_path = os.getcwd()+"/models/quickdraw_model.pth"
+model_path = "/models/quickdraw_model.pth"
 model.load_state_dict(torch.load(model_path, map_location='cpu'))
 model.eval()
 
